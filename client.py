abdcbc6efdca9e3ce04104175fc4dbbb1c2ab62f
kino-ma <ma@kino.ma>
2022-06-16 14:06:47 +0900

    [docs] add some comments

--- client.py
+++ client.py
@@ -2,24 +2,41 @@ import os
 import socket
 import sys
 
+# HOST, PORT を環境変数から取得する
+# 定義されていなかったら、デフォルトの値にする
+# 使い方: $ HOST=localhost PORT=50000 python3 server.py
 HOST = os.environ.get("HOST") or "localhost"
 PORT = int(os.environ.get("PORT") or 50000)
+
+# ソケットでやりとりするデータのサイズ
 BUFSIZE = 4096
 
 
+# メイン関数
 def main():
+    # クライアント用ソケットを作る
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # サーバにアクセスする
     client.connect((HOST, PORT))
 
+    # サーバに送るデータ（あなたの名前）を取得する
+    # コマンドを呼び出した時の第一引数か、パソコンのユーザ名を自動で取得する
     name = sys.argv[1] if len(sys.argv) > 1 else os.environ["USER"]
+
+    # 名前を送信する
     client.sendall(name.encode())
 
+    # レスポンスを受け取る
     data = client.recv(BUFSIZE)
+
+    # 受け取ったデータを画面に表示する
     print(data.decode("UTF-8"), end="")
 
+    # ソケットを閉じる
     client.close()
 
 
+# メイン処理
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,35 +1,51 @@
 import os
 import socket
 
+# HOST, PORT を環境変数から取得する
+# 定義されていなかったら、デフォルトの値にする
+# 使い方: $ HOST=localhost PORT=50000 python3 server.py
 HOST = os.environ.get("HOST") or "localhost"
 PORT = int(os.environ.get("PORT") or 50000)
+
+# ソケットでやりとりするデータのサイズ
 BUFSIZE = 4096
 
 
+# メイン関数
 def main():
+    # サーバ用ソケットを作る
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # PORT を結びつけて、接続を待ち受ける
     server.bind(("", PORT))
     server.listen()
 
     print(f"listening on port {PORT}...")
 
+    # 無限ループ
     while True:
+        # クライアントからのアクセスを受け付ける
         client, addr = server.accept()
         print(f"request received from '{addr[0]}:{addr[1]}'.")
 
+        # クライアントからの入力を受け取る
         name = client.recv(BUFSIZE)
         print(f"name: {name}")
 
+        # 入力と組み合わせてレスポンスを送る
         client.sendall(b"Hi, nice to meet you " + name + b"!\n")
-
         print("response sent!")
+
+        # 接続を切る
         client.close()
         print("connection closed.")
+
         print()
 
+    # 無限ループなのでここまでこないが、一応サーバのソケットを閉じておく
     server.close()
 
 
+# メイン処理
 if __name__ == "__main__":
     main()
